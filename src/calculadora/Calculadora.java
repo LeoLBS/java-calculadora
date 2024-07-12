46ef158b66f5882bc73ca03bd8c991fcba0a3803
LeoLBS <leonardoperin70@gmail.com>
2024-07-12 13:23:43 -0300

    update: java Calculadora.java and PainelCalculadora.java

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -4,6 +4,8 @@ public class Calculadora {
 
     private double resultadoSoma;
     private double resultadoSubtracao;
+    private double resultadoMultiplicacao;
+    private double resultadoDivisao;
 
     public double getSoma(){
         return resultadoSoma;
@@ -16,5 +18,17 @@ public class Calculadora {
     }
     public void setSubtracao(double numero1, double numero2) {
         this.resultadoSubtracao = numero1 - numero2;
+    }
+    public double getMultiplicacao() {
+        return resultadoMultiplicacao;
+    }
+    public void setMultiplicacao(double numero1, double numero2) {
+        this.resultadoMultiplicacao = numero1 * numero2;
+    }
+    public double getDivisao() {
+        return resultadoDivisao;
+    }
+    public void setDivisao(double numero1, double numero2) {
+        this.resultadoDivisao = numero1 / numero2;
     }
 }
--- src/calculadora/PainelCalculadora.java
+++ src/calculadora/PainelCalculadora.java
@@ -1,5 +1,6 @@
 package calculadora;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PainelCalculadora {
@@ -7,17 +8,17 @@ public class PainelCalculadora {
         Calculadora calculadora = new Calculadora();
         Scanner recebe = new Scanner(System.in);
 
-        int opcao;
+        int opcaoEscolha;
         double numero1;
         double numero2;
-
+    try {
         do {
             System.out.println("CALCULADORA");
 
-            System.out.println("Escolha uma opção: 1 - Soma  2 - Subtração  0 - Sair");
-            opcao = recebe.nextInt();
+            System.out.println("Escolha uma opção: 1 - Soma  2 - Subtração  3 - Multiplicação  4 - Divisão  0 - Sair");
+            opcaoEscolha = recebe.nextInt();
 
-            switch (opcao){
+            switch (opcaoEscolha) {
                 case 1:
                     System.out.println("Qual o seu primeiro número: ");
                     numero1 = recebe.nextDouble();
@@ -38,8 +39,31 @@ public class PainelCalculadora {
 
                     System.out.println("Resultado da sua subtração foi de: " + calculadora.getSubtracao());
                     break;
+                case 3:
+                    System.out.println("Qual o seu primeiro número: ");
+                    numero1 = recebe.nextDouble();
+                    System.out.println("Qual o seu segundo número: ");
+                    numero2 = recebe.nextDouble();
+
+                    calculadora.setMultiplicacao(numero1, numero2);
+
+                    System.out.println("Resultado da sua multiplicação foi de: " + calculadora.getMultiplicacao());
+                    break;
+                case 4:
+                    System.out.println("Qual o seu primeiro número: ");
+                    numero1 = recebe.nextDouble();
+                    System.out.println("Qual o seu segundo número: ");
+                    numero2 = recebe.nextDouble();
+
+                    calculadora.setDivisao(numero1, numero2);
+
+                    System.out.println("Resultado da sua multiplicação foi de: " + calculadora.getDivisao());
+                    break;
             }
 
-        }while (opcao != 0);
+        } while (opcaoEscolha != 0);
+    }catch (InputMismatchException e){
+        System.out.println("Valor informado é invalido, para o calculo ser realizar deve ser informado números! Tipo de erro para analise: " + e);
+    }
     }
 }
